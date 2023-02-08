ddeaee983b5a75737b1ae9ab91535c484f6c3d9e
Ivorwen <76967133+Ivorwen@users.noreply.github.com>
2023-02-07 20:11:59 -0500

    Update main.py
    keep is simple stupid

--- main.py
+++ main.py
@@ -1,39 +1,41 @@
+def readGoogleSheet(sheet_id, sheet_name):
+  # Gets and returns data from a google sheet
+  sheetId = sheet_id
+  sheetName = sheet_name
+  url = f"https://docs.google.com/spreadsheets/d/{sheetId}/gviz/tq?tqx=out:csv&sheet={sheetName}"
 
-rounds = 4
-playernames = ["a", "b", "c", "d"]
-listofplayers = [None for x in range(len(playernames))]
+  sheetData = pd.read_csv(url)
 
-def getResultsInto(playerlist, roundnumber):
-	return
-
-
-#sort from lowest to highest score
-def sortByRating(playerlist):
-	for i in range(0, len(listofplayers), 2):
-		if (listofplayers[i+1].level < listofplayers[i].level):
-			tmp = listofplayers[i]
-			listofplayers[i] = listofplayers[i+1]
-			listofplayers[i+1] = tmp
-	return
-
-class Player:
-	def __init__(self, name):
-		global rounds
-		self.name = name
-		self.level = rounds
-		self.playersPlayed = [None for x in range(len(playernames))]
-
-for i in range(0, len(playernames)):
-	listofplayers[i] = Player(playernames[i])
-
-roundnumber = 1
-print("round "+str(roundnumber))
-for i in range(0,len(listofplayers), 2):
-	print(listofplayers[i].name, "plays against",listofplayers[i+1].name)
-	listofplayers[i].playersPlayed.append(listofplayers[i+1])
-	listofplayers[i+1].playersPlayed.append(listofplayers[i])
-
-getResultsInto(listofplayers, roundnumber)
-sortByRating(listofplayers)
+  return sheetData
 
 
+def makeScript(sheet_data):
+  # Write script for https://coronate.netlify.app/players
+
+  dataText = '{"config": {"avoidPairs": [], "byeValue": 1, "lastBackup": "1970-01-01T00:00:00.000Z"}, "players": {'
+
+  for row in range(len(sheet_data)):
+      firstName = sheet_data.iloc[row,0].strip()   #strip trailing spaces
+      lastName = sheet_data.iloc[row,1].strip()    #strip trailing spaces
+      id = firstName + "-" + lastName
+      dataText = dataText + '"{id}": {{'.format(id=id)
+      dataText = dataText + '"firstName": "{firstName}", "id": "{id}", "lastName": "{lastName}", "matchCount": 0, "rating": 1, "type_": "person"'\
+      .format(firstName=firstName, id=id, lastName=lastName)
+      dataText = dataText + '}'
+
+      if row != len(sheet_data)-1:                 #don't add a comma after the last item
+        dataText = dataText + ','
+
+  dataText = dataText + '}, "tournaments": {}}'
+
+  return dataText
+
+
+def main():
+  import pandas as pd
+  names = readGoogleSheet("1X1rPI1x6GYm2mRRofPEyDdCNArYjO7mWQefBOTK5xxc", "Names")
+  text = makeScript(names)
+  print(text)
+
+
+main()
